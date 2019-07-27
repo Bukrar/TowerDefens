3ba827456fff752a50309dc6c995202c9937cb68
Bukrar <alex968624229@gmail.com>
2019-07-27 16:29:41 +0800

    ADD PathFiner.cs 把路徑點加進Dic裡

---
+++ TowerDefens/Assets/PathFiner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFiner : MonoBehaviour
+{
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadBlock();
+    }
+
+    private void LoadBlock()
+    {
+        var wayPoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in wayPoints)
+        {
+            bool isOverlapping = grid.ContainsKey(waypoint.GetGridPos());
+            if (isOverlapping)
+            {
+                print("LAP");
+            }
+            else {
+                grid.Add(waypoint.GetGridPos(), waypoint);
+            }
+        }
+        print(grid.Count);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
