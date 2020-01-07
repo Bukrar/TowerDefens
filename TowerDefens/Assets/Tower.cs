b2ebb7769620f643645329a6a3abd04861fd2310
Bukrar <alex968624229@gmail.com>
2020-01-07 23:21:26 +0800

    Shoot Eneny And Destory Enemy Check Distance

--- TowerDefens/Assets/Tower.cs
+++ TowerDefens/Assets/Tower.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,39 @@ public class Tower : MonoBehaviour
 {
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform targetEnemy;
+
+    [SerializeField] float attactRange = 30f;
+    [SerializeField] ParticleSystem projectileParticle;
     // Update is called once per frame
     void Update()
     {
-        objectToPan.LookAt(targetEnemy);
+        if (targetEnemy)
+        {
+            objectToPan.LookAt(targetEnemy);
+            FireEnemy();
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void FireEnemy()
+    {
+        float distanceToEnemy = Vector3.Distance(targetEnemy.transform.position, this.gameObject.transform.position);
+        if (distanceToEnemy <= attactRange)
+        {
+            Shoot(true);
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void Shoot(bool isActive)
+    {
+        var emissionModule = projectileParticle.emission;
+        emissionModule.enabled = isActive;
     }
 }
